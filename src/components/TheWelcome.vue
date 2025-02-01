486e12537c80145bce0f5be156a1289d64cd4340
Matt Kuhn <mlkkuhn@live.com>
2025-02-01 13:36:30 -0500

    init

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+import WelcomeItem from './WelcomeItem.vue'
+import CameraIcon from './icons/IconCamera.vue'
+import UploadIcon from './icons/IconUpload.vue'
+import ReviewIcon from './icons/IconReview.vue'
+import CompleteIcon from './icons/IconComplete.vue'
+import EditIcon from './icons/IconEdit.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <CameraIcon />
+    </template>
+    <template #heading>Photo</template>
+
+    Take a picture of your attendance sheet.
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <UploadIcon />
+    </template>
+    <template #heading>Upload</template>
+
+    Upload the picture, here, to the system.
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <ReviewIcon />
+    </template>
+    <template #heading>Review</template>
+    
+    Ensure the data we extracted is accurate.
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <EditIcon />
+    </template>
+    <template #heading>Edit</template>
+
+    Edit if necessary.
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <CompleteIcon />
+    </template>
+    <template #heading>Complete</template>
+
+    Approve & complete!
+  </WelcomeItem>
+</template>
---
+++ src/components/icons/IconComplete.vue
@@ -0,0 +1,11 @@
+<template>
+<svg fill="#000000" width="800px" height="800px" viewBox="-960 -960 3840.00 3840.00" xmlns="http://www.w3.org/2000/svg">
+
+<g id="SVGRepo_bgCarrier" stroke-width="0"/>
+
+<g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"/>
+
+<g id="SVGRepo_iconCarrier"> <path d="M960 1807.059c-467.125 0-847.059-379.934-847.059-847.059 0-467.125 379.934-847.059 847.059-847.059 467.125 0 847.059 379.934 847.059 847.059 0 467.125-379.934 847.059-847.059 847.059M960 0C430.645 0 0 430.645 0 960s430.645 960 960 960 960-430.645 960-960S1489.355 0 960 0M854.344 1157.975 583.059 886.69l-79.85 79.85 351.135 351.133L1454.4 717.617l-79.85-79.85-520.206 520.208Z" fill-rule="evenodd"/> </g>
+
+</svg>
+</template>
